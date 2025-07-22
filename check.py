e2945ee8891fcd7b3e747684db949c362e2d57db
joyencheah <joyen.cheah@f-secure.com>
2025-07-22 09:34:24 +0800

    testing1- test

--- check.py
+++ check.py
@@ -1,3 +1,7 @@
+# def login(username, password):
+#     query = f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
+#     return db.execute(query).fetchone()
+
 def login(username, password):
     query = f"SELECT * FROM users WHERE username='{username}' AND password='{password}'"
     return db.execute(query).fetchone()
